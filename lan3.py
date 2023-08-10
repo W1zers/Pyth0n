3379736ece7defb2ff182109b0c141296f45c1f4
Ramin Gambarov <ramin.gambarov2014@gmail.com>
2023-08-10 20:46:19 +0300

    info

---
+++ lan3.py
@@ -0,0 +1,21 @@
+# Требуется вывести все целые степени двойки (т.е. числа вида 2k), не превосходящие числа N.
+
+# n = int(input('Введи число N:'))
+# i = 0
+# while 2 ** i <= n:
+#     print(f' 2 в степени {i} равна {2 ** i}')
+#     i += 1
+
+N = abs(int(input('Введите число N ')))
+stop = 0
+P = 2
+for i in range(N):
+    if stop != 1:
+        P = P ** i
+        if P <= N:
+            print(P, end=' ')
+            P = 2
+        else:
+            stop = 1
+    else:
+        i = N
