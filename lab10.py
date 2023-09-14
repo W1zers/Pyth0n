a42b4c55d9f08ef034e08d48f9ecafe371ffed28
Ramin Gambarov <ramin.gambarov2014@gmail.com>
2023-09-14 19:00:21 +0300

    info

---
+++ lab10.py
@@ -0,0 +1,75 @@
+import os, re
+
+
+def phone_format(n):  # форматирование телефонного номера
+    n = n.removeprefix("+")
+    n = re.sub("[ ()-]", "", n)
+    return format(int(n[:-1]), ",").replace(",", "-") + n[-1]
+
+
+def printData(data):  # Функция вывода телефонной книги в консоль
+    phoneBook = []
+    splitLine = "=" * 49
+    print(splitLine)
+    print(" №  Lastname        Name          Phone Numbers")
+    print(splitLine)
+    personID = 1
+
+    for contact in data:
+        lastName, name, phone = contact.rstrip().split(",")
+        phoneBook.append(
+            {
+                "ID": personID,
+                "lastName": lastName,
+                "name": name,
+                "phone": phone_format(phone),
+            }
+        )
+        personID += 1
+
+    for contact in phoneBook:
+        personID, lastName, name, phone = contact.values()
+        print(f"{personID:>2}. {lastName:<15} {name:<10} -- {phone:<15}")
+
+    print(splitLine)
+
+
+def showContacts(fileName):  # Функция открытия телефонной книги
+    os.system("cls")
+    phoneBook = []
+    with open(fileName, "r", encoding="UTF-8") as file:
+        data = sorted(file.readlines())
+        printData(data)
+    input("\n--- press any key ---")
+
+
+def addContact(fileName):  # Функция добавления нового контакта в телефонную книгу
+    os.system("cls")
+    with open(fileName, "a", encoding="UTF-8") as file:
+        res = ""
+        res += input("Input a Surname of Contact: ") + ","
+        res += input("Input a Name of Contact: ") + ","
+        res += input("Input a Phone Number of Contact: ")
+
+        file.write(res + "\n")
+
+    input("\nContact was successfully added!\n--- press any key ---")
+
+
+def findContact(fileName):  # Функция поиска контактов в телефонной книге
+    os.system("cls")
+    target = input("Input Item of Contact for searching: ")
+    result = []
+    with open(fileName, "r", encoding="UTF-8") as file:
+        data = file.readlines()
+        for person in data:
+            if target in person:
+                result.append(person)
+                # break
+
+    if len(result) != 0:
+        printData(result)
+    else:
+        print(f"There is no Contact with this Item '{target}'.")
+
+    input("--- press any key ---")
